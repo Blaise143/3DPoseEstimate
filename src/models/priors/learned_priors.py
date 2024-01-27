11ec965698a116ec4d4f2752bf84bf2e3a33c0ea
Blaise143 <blaiseappol@gmail.com>
2024-01-27 15:38:53 -0800

    used kl_divergence from torch distributions

--- src/models/priors/learned_priors.py
+++ src/models/priors/learned_priors.py
@@ -2,13 +2,19 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
 import pytorch_lightning as pl
+from torch.distributions import Normal, kl_divergence
 import wandb
+
 
 BATCH_SIZE = 500
 
 
 class VariationalAutoEncoder(pl.LightningModule):
-    def __init__(self, layers_order: list, latent_dim: int = 10, dataset=None, dropout=0.3, learning_rate=3e-4) -> None:
+
+    def __init__(self, layers_order: list,
+                 latent_dim: int = 10,
+                 dataset=None, dropout=0.3,
+                 learning_rate=3e-4) -> None:
         super().__init__()
         self.latent_dim = latent_dim
         self.learning_rate = learning_rate
@@ -40,12 +46,12 @@ class VariationalAutoEncoder(pl.LightningModule):
             decode_layers.append(
                 nn.Linear(layers_order[i], layers_order[i + 1]))
             if i < len(layers_order) - 2:
-                decode_layers.append(nn.BatchNorm1d(layers_order[i+1]))
+                # decode_layers.append(nn.BatchNorm1d(layers_order[i+1]))
                 decode_layers.append(nn.ReLU())
                 decode_layers.append(nn.Dropout(dropout))
         self.decoder = nn.Sequential(*decode_layers)
 
-        self.reconstruction_loss = nn.MSELoss(reduction="sum")
+        self.reconstruction_loss = nn.MSELoss(reduction="mean")
 
     def encode(self, x: torch.Tensor):
         x = self.encoder(x)
@@ -67,52 +73,71 @@ class VariationalAutoEncoder(pl.LightningModule):
         return reconstructed, mu, log_var
 
     def training_step(self, batch: torch.Tensor, batch_idx: int):
-        x, x_v = batch
-        reconstructed, mu, log_var = self.forward(x)
-        # Calculating reconstruction loss
-        recon_loss = self.reconstruction_loss(reconstructed, x)
-        # calculating kl divergence loss
-        kl_divergence_loss = self.kl_divergence(mu, log_var)
 
-        reconstructed_v, mu_v, log_var_v = self.forward(x_v)
-        recon_loss_v = self.reconstruction_loss(reconstructed_v, x_v)
-        kl_divergence_loss_v = self.kl_divergence(mu_v, log_var_v)
+        losses = self._common_step(batch, batch_idx)
+
+        recon_loss = 3*losses["recon_loss"]
+        kl_divergence_loss = losses["kl_divergence_loss"]
+
+        recon_loss_v = 3*losses["recon_loss_v"]
+        kl_divergence_loss_v = losses["kl_divergence_loss_v"]
 
         loss = recon_loss+kl_divergence_loss + recon_loss_v + kl_divergence_loss_v
-        self.log("train_loss", loss)
-        self.log("train_reconstruction_loss", recon_loss+recon_loss_v)
+        self.log("train_loss", loss, on_epoch=True, on_step=False)
+        self.log("train_reconstruction_loss", recon_loss +
+                 recon_loss_v, on_epoch=True, on_step=False)
         self.log("train_kl_divergence_loss",
-                 kl_divergence_loss+kl_divergence_loss_v)
+                 kl_divergence_loss+kl_divergence_loss_v, on_epoch=True, on_step=False)
 
         return loss
 
     def validation_step(self, batch: torch.Tensor, batch_idx: int):
-        x, x_v = batch
-        reconstructed, mu, logvar = self.forward(x)
-        reconstructed_v, mu_v, logvar_v = self.forward(x_v)
-        # loss = self.loss_function(
-        # x=x, x_reconstructed=reconstructed, mu=mu, log_var=logvar)
-        recon_loss = self.reconstruction_loss(reconstructed, x)
-        kl_divergence_loss = self.kl_divergence(mu, logvar)
 
-        recon_loss_v = self.reconstruction_loss(reconstructed_v, x_v)
-        kl_divergence_loss_v = self.kl_divergence(mu_v, logvar_v)
+        losses = self._common_step(batch, batch_idx)
+
+        recon_loss = 3*losses["recon_loss"]
+        kl_divergence_loss = losses["kl_divergence_loss"]
+
+        recon_loss_v = 3*losses["recon_loss_v"]
+        kl_divergence_loss_v = losses["kl_divergence_loss_v"]
 
         loss = recon_loss+kl_divergence_loss + recon_loss_v+kl_divergence_loss_v
 
-        self.log("val_loss", loss)
-        self.log("val_reconstruction_loss", recon_loss + recon_loss_v)
+        self.log("val_loss", loss, on_epoch=True)
+        self.log("val_reconstruction_loss", recon_loss +
+                 recon_loss_v, on_epoch=True, on_step=False)
         self.log("val_kl_divergence_loss",
-                 kl_divergence_loss + kl_divergence_loss_v)
+                 kl_divergence_loss + kl_divergence_loss_v, on_epoch=True, on_step=False)
 
         return loss
 
-    def kl_divergence(self, mu: torch.Tensor, log_var: torch.Tensor):
-        var = log_var.exp()
-        mu_squared = mu.pow(2)
-        kl_div_elementwise = 1 + log_var - mu_squared - var
-        total_kl_div = -0.5 * torch.sum(kl_div_elementwise)
-        return total_kl_div
+    def _common_step(self, batch: torch.Tensor, batch_idx: int):
+        x, x_v = batch
+        reconstructed, mu, logvar = self.forward(x)
+        reconstructed_v, mu_v, logvar_v = self.forward(x_v)
+
+        # for x
+        recon_loss = self.reconstruction_loss(reconstructed, x)
+        kl_divergence_loss = 0.2*self.kl_divergence_loss(mu, logvar)
+
+        # for virtual
+        recon_loss_v = self.reconstruction_loss(reconstructed_v, x_v)
+        kl_divergence_loss_v = 0.2 * self.kl_divergence_loss(mu_v, logvar_v)
+
+        losses = {
+            "recon_loss": recon_loss,
+            "kl_divergence_loss": kl_divergence_loss,
+
+            "recon_loss_v": recon_loss_v,
+            "kl_divergence_loss_v": kl_divergence_loss_v
+        }
+        return losses
+
+    def kl_divergence_loss(self, mu: torch.Tensor, log_var: torch.Tensor):
+        posterior = Normal(mu, log_var.exp().sqrt())
+        prior = Normal(torch.zeros_like(mu), torch.ones_like(log_var))
+        kl_div = kl_divergence(posterior, prior).mean()
+        return kl_div
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
